e83b77cd80a95e3d29b23711b8b0f4003452b387
Oliver Gao <oliver.gao@servian.com>
2022-03-23 22:42:08 +1100

    fix 000

--- 000_getting_started/main.tf
+++ 000_getting_started/main.tf
@@ -1,21 +1,20 @@
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "ExamPro"
-
-    workspaces {
-      name = "getting-started"
-    }
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.58.0"
-    }
-  }
+locals {
+  project_name = "Oliver"
 }
 
-locals {
-  project_name = "Andrew"
+terraform {
+  cloud {
+    organization = "olivergao"
+
+    workspaces {
+      name = "learn-terraform"
+    }
+  }
+
+required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.6.0"
+    }
+  }
 }
--- 000_getting_started/providers.tf
+++ 000_getting_started/providers.tf
@@ -1,12 +1,3 @@
-
-
 provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "eu-west-1"
-	alias   = "eu"
+    region = "ap-southeast-2"
 }
